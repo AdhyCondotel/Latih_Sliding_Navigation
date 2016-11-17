ff59f656bd5d0ba496abf557807f7c12f4463c0e
adhy condotel <https://Ritelaku@bitbucket.org/camarateam/karebosi-android.git>
2016-11-17 17:17:39 +0800

    Initial commit

---
+++ app/src/main/java/com/android/latih_sliding_navigation/Fragment/MainFragment_One.java
@@ -0,0 +1,23 @@
+package com.android.latih_sliding_navigation.Fragment;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.latih_sliding_navigation.R;
+
+public class MainFragment_One extends Fragment {
+
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            ViewGroup grup = (ViewGroup) inflater.inflate(R.layout.activity_main_fragment_one,null);
+
+            TextView txt = (TextView) grup.findViewById(R.id.textView);
+            txt.setText("Sliding One");
+            return grup;
+
+
+    }
+}
---
+++ app/src/main/java/com/android/latih_sliding_navigation/MainActivity_Menu.java
@@ -0,0 +1,114 @@
+package com.android.latih_sliding_navigation;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.android.latih_sliding_navigation.Sliding.SlidingTabLayout;
+
+public class MainActivity_Menu extends AppCompatActivity {
+
+
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+
+    private ViewPager pager;
+    ViewPagerAdapter adapter;
+    SlidingTabLayout tabs;
+    CharSequence Titles[]={"Tab 1","Tab 2","Tab 3"};
+    int Numboftabs =3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__menu);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabScrollColor);
+            }
+        });
+
+        tabs.setViewPager(pager);
+
+
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if(menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+
+                //Closing drawer on item click
+                drawerLayout.closeDrawers();
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()){
+                    case R.id.website:
+                        Toast.makeText(getApplicationContext(),"Pilih 1 ",Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    case R.id.email:
+                        Toast.makeText(getApplicationContext(),"Pilih 2 ",Toast.LENGTH_SHORT).show();
+
+                        return true;
+
+                     default:
+                        Toast.makeText(getApplicationContext(),"Tidak ada yang terpilih",Toast.LENGTH_SHORT).show();
+                        return true;
+
+                }
+            }
+        });
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar, R.string.openDrawer, R.string.closeDrawer){
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessay or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+
+    }
+}
